2421790adcfebf8d0acb09f76dab3261d2b7b5cf
MiBBy-4 <maksim.762373maks@bk.ru>
2022-03-01 13:46:53 +0300

    feat: change table and add ability go to the personal page of idea

--- src/Idea/IdeaList.jsx
+++ src/Idea/IdeaList.jsx
@@ -1,49 +1,55 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { DataGrid } from '@mui/x-data-grid';
-import IdeaItem from './IdeaItem';
-import { getIdeas } from '../apiRequests/IdeasRequests';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { getIdeas, changeNumberOfViews } from '../apiRequests/IdeasRequests';
 
 function IdeaList() {
   const [items, setItems] = useState([]);
-  const columns = [
-    {
-      field: 'id',
-      headerName: 'ID',
-      width: 70,
-      identity: true,
-    },
-    {
-      field: 'name',
-      headerName: 'Name',
-      width: 130,
-    },
-    {
-      field: 'description',
-      headerName: 'Description',
-      width: 130,
-    },
-    {
-      field: 'problem',
-      headerName: 'Problem',
-      width: 130,
-    },
-  ];
 
   useEffect(async () => {
     const response = await getIdeas();
     setItems(response.data.reverse());
   });
 
+  async function handleClick(idea) {
+    await changeNumberOfViews(idea);
+  }
+
   return (
     <div style={{ height: 400, width: '100%' }}>
-      <DataGrid
-        rows={items}
-        columns={columns}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
-        getRowId={(row) => row.id}
-      />
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="right">Id</TableCell>
+              <TableCell align="right">Name</TableCell>
+              <TableCell align="right">Description</TableCell>
+              <TableCell align="right">Problem</TableCell>
+              <TableCell align="right">Action</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {items.map((item) => (
+              <TableRow
+                key={item.id}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell align="right">{item.id}</TableCell>
+                <TableCell align="right">{item.name}</TableCell>
+                <TableCell align="right">{item.description}</TableCell>
+                <TableCell align="right">{item.problem}</TableCell>
+                <TableCell align="right"><Link to={`/ideas/${item.id}`} onClick={() => handleClick(item)}>View</Link></TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 }
